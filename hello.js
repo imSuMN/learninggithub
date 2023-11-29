894746b340bc4a7b2c158de0f3f880c7898b0389
Suman Kumar <suman.kumar.it.2020@tint.edu.in>
2023-11-29 16:14:22 +0530

    changes in the first file in the branch -> suman2.

--- hello.js
+++ hello.js
@@ -2,6 +2,9 @@ console.log("helo world")
 
 function(){
   for(var i = 0 ;  i < 10 ; i++){
-    console.log(i)
+    console.log(i);
   }
 }
+
+
+// This line is added in the file from the branch -> suman2
